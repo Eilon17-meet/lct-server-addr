ae70986a1e244b2d5118b53ae00c96f33b8ee9b3
Eilon17-meet <eilon17@meet.mit.edu>
2018-12-08 15:40:56 +0000

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_server_addr():
-    return 'http://109.186.174.98'
+    # return 'http://109.186.174.98'
+    return 'https://lct.localcloudtech.com'
 
 
 if __name__ == '__main__':
