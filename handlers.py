101e34139ba71f1c77ecc54ae3afb2d917adc7a7
Eilon17-meet <eilon17@meet.mit.edu>
2018-12-11 12:52:56 +0000

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,8 +1,9 @@
 '''
 1. Update the ip address in the function get_server_addr().
-2. Go to https://dashboard.heroku.com/apps/localcloudtech/deploy/github (heroku website).
-3. Go down the page and click "Deploy Branch".
-4. Wait until it's done.
+2. Click "Commit changes" green button (down this page).
+3. Go to https://dashboard.heroku.com/apps/localcloudtech/deploy/github (heroku website).
+4. Go down the page and click "Deploy Branch".
+5. Wait until it's done.
 '''
 from flask import Flask
 
